de0f944248d58493c63875d390995da06b4c6048
Mike Looijmans <mike.looijmans@topic.nl>
2014-10-29 08:51:48 +0100

    drivers/regulator/ltc3562.c: Mainline submission fixup
    Prepare the driver for mainline kernel submission:
    - Rename DT properties
    - Add DT description
    - use devm_regulator_register
    - Whitespace and other coding style adjustments
    - Drop default-voltage property, must use min==max
    - Add .of_match_table and prefix lltc
    - Clarify why regmap cannot be used
    - Add lltc,operating-mode (0..3) DT property
    - Prevent on-off-on transient when boot-on is set

--- drivers/regulator/ltc3562.c
+++ drivers/regulator/ltc3562.c
@@ -1,5 +1,5 @@
 /*
- * Regulator driver for Linear Technology LCT3562 
+ * Regulator driver for Linear Technology LTC3562
  *
  *  Copyright (C) 2014 Topic Embedded Products
  *
@@ -7,7 +7,6 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
-
 #include <linux/bug.h>
 #include <linux/err.h>
 #include <linux/i2c.h>
@@ -18,24 +17,23 @@
 #include <linux/slab.h>
 #include <linux/regulator/of_regulator.h>
 
+#define LTC3562_NUM_REGULATORS	4
 
-#define LTC3562_NUM_REGULATORS	4 
+#define REGULATOR_TYPE_A_NUM_VOLTAGES	16
+#define REGULATOR_TYPE_A_MIN_UV	425000
+#define REGULATOR_TYPE_A_UV_STEP	25000
 
-#define REGULATOR_TYPE_A_NUM_VOLTAGES	    16
-#define REGULATOR_TYPE_A_MIN_UV		425000
-#define REGULATOR_TYPE_A_UV_STEP	 25000
+#define REGULATOR_TYPE_B_NUM_VOLTAGES	128
+#define REGULATOR_TYPE_B_MIN_UV	600000
+#define REGULATOR_TYPE_B_UV_STEP	25000
 
-#define REGULATOR_TYPE_B_NUM_VOLTAGES	   128
-#define REGULATOR_TYPE_B_MIN_UV		600000
-#define REGULATOR_TYPE_B_UV_STEP	 25000
-
-
+/* the LTC3562 does not have a register map, instead it receives a two-byte
+ * command set. The first byte sets the mask for the output(s) to be programmed
+ * and the second byte hold the "enable" bit and the DAC code. */
 struct ltc3562_status {
-	u8 shadow_addr;		/* Shadow register of address byte */
-	u8 shadow_data;		/* Shadow register of data byte */
-	u8 voltage_set;		/* Check if voltage has been set before */
+	u8 addr_mode;	/* sub-address byte: program mask an operating mode */
+	u8 enable_daccode;	/* data byte: Enable bit and DAC code  */
 };
-
 
 struct ltc3562 {
 	struct device *dev;
@@ -43,11 +41,7 @@ struct ltc3562 {
 	struct mutex mutex;
 	struct regulator_dev *rdev[LTC3562_NUM_REGULATORS];
 	struct ltc3562_status rstatus[LTC3562_NUM_REGULATORS];
-	
 };
-
-static int ltc3562_write(struct i2c_client *i2c, u8 reg_a, u8 reg_b);
-static int ltc3562_dummy_write(struct i2c_client *i2c);
 
 #define LTC3562_R400B_ID	0x00
 #define LTC3562_R600B_ID	0x01
@@ -63,59 +57,22 @@ static int ltc3562_dummy_write(struct i2c_client *i2c);
 
 #define OPERATING_MODE_PULSE_SKIP	0x00
 #define OPERATING_MODE_LDO		0x01
-#define OPERATING_MODE_FOCED_BURST	0x02
+#define OPERATING_MODE_FORCED_BURST	0x02
 #define OPERATING_MODE_BURST		0x03
 
 #define REGULATOR_ENABLE_BIT	0x80
 
-
-static int ltc3562_regulator_enable(struct regulator_dev *dev){
-	int ret, v_index;
-	unsigned int v_default;
-	struct ltc3562 *ltc3562 = rdev_get_drvdata(dev);
-	int id = rdev_get_id(dev);
-	struct ltc3562_status *status = &ltc3562->rstatus[id];
-
-	mutex_lock(&ltc3562->mutex);
-
-	if(!status->voltage_set){
-		if(of_property_read_u32(dev->dev.of_node, "ltc3562-default-voltage", &v_default) == 0){
-			v_index = dev->desc->ops->map_voltage(dev,v_default , v_default);
-			if(v_index > 0){
-				status->shadow_data = v_index & (~REGULATOR_ENABLE_BIT);
-			}
-			status->voltage_set = 1;
-		}
-
-	}
-
-	status->shadow_data |= REGULATOR_ENABLE_BIT;
-
-	ret = ltc3562_write(ltc3562->i2c, status->shadow_addr | PROGRAM_R400B_MASK << id, status->shadow_data);
-
-	mutex_unlock(&ltc3562->mutex); 
-
-	return ret;
+static int ltc3562_update(struct ltc3562 *ltc3562,
+	struct ltc3562_status *status)
+{
+	dev_dbg(&ltc3562->i2c->dev, "send %#x %#x\n",
+		status->addr_mode, status->enable_daccode);
+	return i2c_smbus_write_byte_data(
+		ltc3562->i2c, status->addr_mode, status->enable_daccode);
 }
 
-static int ltc3562_regulator_disable(struct regulator_dev *dev){
-	int ret;
-	struct ltc3562 *ltc3562 = rdev_get_drvdata(dev);
-	int id = rdev_get_id(dev);
-	struct ltc3562_status *status = &ltc3562->rstatus[id];
-
-	mutex_lock(&ltc3562->mutex);
-	
-	status->shadow_data &= ~(REGULATOR_ENABLE_BIT);
-
-	ret = ltc3562_write(ltc3562->i2c, status->shadow_addr, status->shadow_data);
-
-	mutex_unlock(&ltc3562->mutex); 
-
-	return ret;
-}
-
-static int ltc3562_regulator_is_enabled(struct regulator_dev *dev){
+static int ltc3562_regulator_enable(struct regulator_dev *dev)
+{
 	int ret;
 	struct ltc3562 *ltc3562 = rdev_get_drvdata(dev);
 	int id = rdev_get_id(dev);
@@ -123,42 +80,78 @@ static int ltc3562_regulator_is_enabled(struct regulator_dev *dev){
 
 	mutex_lock(&ltc3562->mutex);
 
-	ret = (status->shadow_data & REGULATOR_ENABLE_BIT) ? 1 : 0;
+	status->enable_daccode |= REGULATOR_ENABLE_BIT;
+	ret = ltc3562_update(ltc3562, status);
 
 	mutex_unlock(&ltc3562->mutex);
 
-	return ret; 
+	return ret;
 }
 
-static int ltc3562_set_voltage_sel(struct regulator_dev *dev, unsigned int selector){
+static int ltc3562_regulator_disable(struct regulator_dev *dev)
+{
 	int ret;
 	struct ltc3562 *ltc3562 = rdev_get_drvdata(dev);
 	int id = rdev_get_id(dev);
 	struct ltc3562_status *status = &ltc3562->rstatus[id];
 
 	mutex_lock(&ltc3562->mutex);
-	
-	status->shadow_data = (status->shadow_data & REGULATOR_ENABLE_BIT) | selector;
-	status->voltage_set = 1;
 
-	ret = ltc3562_write(ltc3562->i2c, status->shadow_addr, status->shadow_data);
+	status->enable_daccode &= ~(REGULATOR_ENABLE_BIT);
+	ret = ltc3562_update(ltc3562, status);
 
-	mutex_unlock(&ltc3562->mutex); 
+	mutex_unlock(&ltc3562->mutex);
 
 	return ret;
 }
 
-static int ltc3562_get_voltage_sel(struct regulator_dev *dev){
+static int ltc3562_regulator_is_enabled(struct regulator_dev *dev)
+{
 	int ret;
 	struct ltc3562 *ltc3562 = rdev_get_drvdata(dev);
 	int id = rdev_get_id(dev);
 	struct ltc3562_status *status = &ltc3562->rstatus[id];
 
 	mutex_lock(&ltc3562->mutex);
-	
-	ret = status->shadow_data & ~(REGULATOR_ENABLE_BIT);
 
-	mutex_unlock(&ltc3562->mutex); 
+	ret = (status->enable_daccode & REGULATOR_ENABLE_BIT) ? 1 : 0;
+
+	mutex_unlock(&ltc3562->mutex);
+
+	return ret;
+}
+
+static int ltc3562_set_voltage_sel(struct regulator_dev *dev,
+	unsigned int selector)
+{
+	int ret;
+	struct ltc3562 *ltc3562 = rdev_get_drvdata(dev);
+	int id = rdev_get_id(dev);
+	struct ltc3562_status *status = &ltc3562->rstatus[id];
+
+	mutex_lock(&ltc3562->mutex);
+
+	status->enable_daccode =
+		(status->enable_daccode & REGULATOR_ENABLE_BIT) | selector;
+	ret = ltc3562_update(ltc3562, status);
+
+	mutex_unlock(&ltc3562->mutex);
+
+	return ret;
+}
+
+static int ltc3562_get_voltage_sel(struct regulator_dev *dev)
+{
+	int ret;
+	struct ltc3562 *ltc3562 = rdev_get_drvdata(dev);
+	int id = rdev_get_id(dev);
+	struct ltc3562_status *status = &ltc3562->rstatus[id];
+
+	mutex_lock(&ltc3562->mutex);
+
+	ret = status->enable_daccode & ~(REGULATOR_ENABLE_BIT);
+
+	mutex_unlock(&ltc3562->mutex);
 
 	return ret;
 }
@@ -173,14 +166,13 @@ static struct regulator_ops ltc3562_regulator_ops = {
 	.map_voltage = regulator_map_voltage_linear,
 };
 
-
 static struct regulator_desc ltc3562_regulators[] = {
 	{
 		.name = "R400B",
 		.id = LTC3562_R400B_ID,
 		.ops = &ltc3562_regulator_ops,
 		.min_uV = REGULATOR_TYPE_B_MIN_UV,
-		.uV_step = REGULATOR_TYPE_B_UV_STEP, 
+		.uV_step = REGULATOR_TYPE_B_UV_STEP,
 		.n_voltages = REGULATOR_TYPE_B_NUM_VOLTAGES,
 		.type = REGULATOR_VOLTAGE,
 		.owner = THIS_MODULE,
@@ -190,7 +182,7 @@ static struct regulator_desc ltc3562_regulators[] = {
 		.id = LTC3562_R600B_ID,
 		.ops = &ltc3562_regulator_ops,
 		.min_uV = REGULATOR_TYPE_B_MIN_UV,
-		.uV_step = REGULATOR_TYPE_B_UV_STEP, 
+		.uV_step = REGULATOR_TYPE_B_UV_STEP,
 		.n_voltages = REGULATOR_TYPE_B_NUM_VOLTAGES,
 		.type = REGULATOR_VOLTAGE,
 		.owner = THIS_MODULE,
@@ -200,7 +192,7 @@ static struct regulator_desc ltc3562_regulators[] = {
 		.id = LTC3562_R400A_ID,
 		.ops = &ltc3562_regulator_ops,
 		.min_uV = REGULATOR_TYPE_A_MIN_UV,
-		.uV_step = REGULATOR_TYPE_A_UV_STEP, 
+		.uV_step = REGULATOR_TYPE_A_UV_STEP,
 		.n_voltages = REGULATOR_TYPE_A_NUM_VOLTAGES,
 		.type = REGULATOR_VOLTAGE,
 		.owner = THIS_MODULE,
@@ -210,131 +202,128 @@ static struct regulator_desc ltc3562_regulators[] = {
 		.id = LTC3562_R600A_ID,
 		.ops = &ltc3562_regulator_ops,
 		.min_uV = REGULATOR_TYPE_A_MIN_UV,
-		.uV_step = REGULATOR_TYPE_A_UV_STEP, 
+		.uV_step = REGULATOR_TYPE_A_UV_STEP,
 		.n_voltages = REGULATOR_TYPE_A_NUM_VOLTAGES,
 		.type = REGULATOR_VOLTAGE,
 		.owner = THIS_MODULE,
 	},
 };
 
-
-static int ltc3562_write(struct i2c_client *i2c, u8 reg_a, u8 reg_b){
-	return i2c_smbus_write_byte_data(i2c, reg_a, reg_b);
+/* Write dummy data to detect presence of the physical device. */
+static int ltc3562_dummy_write(struct i2c_client *i2c)
+{
+	return i2c_smbus_write_byte_data(i2c, 0, 0);
 }
 
-/* Write dummy data to detect presence of physical device */
-static int ltc3562_dummy_write(struct i2c_client *i2c){
-	return ltc3562_write(i2c, 0, 0);
-}
-
-
-static int ltc3562_regulators_unregister(struct ltc3562 *ltc3562, int num_regulators){
-	while(--num_regulators >= 0){
-		pr_debug("Unregistering regulator: %s\n", ltc3562->rdev[num_regulators]->desc->name);
-		regulator_unregister(ltc3562->rdev[num_regulators]);
-	}
-	return 0;
-}
-
-static int ltc3562_i2c_probe(struct i2c_client *i2c, const struct i2c_device_id *id){
+static int ltc3562_i2c_probe(struct i2c_client *i2c,
+	const struct i2c_device_id *id)
+{
 	int i, error;
-	unsigned int r1, r2;
 	unsigned long uV;
 	struct ltc3562 *ltc3562;
 	struct regulator_config rconfig = { };
 	struct device_node *np_regulators, *np;
 	struct device_node *np_child;
+	u32 value;
 
-
-	ltc3562 = devm_kzalloc(&i2c->dev, sizeof(struct ltc3562), GFP_KERNEL);
+	ltc3562 = devm_kzalloc(&i2c->dev,
+		sizeof(struct ltc3562), GFP_KERNEL);
 	if (ltc3562 == NULL)
 		return -ENOMEM;
 
 	ltc3562->i2c = i2c;
+	i2c_set_clientdata(i2c, ltc3562);
 
 	mutex_init(&ltc3562->mutex);
 
-	if(ltc3562_dummy_write(i2c) < 0){
-		dev_err(&i2c->dev, "Could not find device LTC3562 on i2c bus!\n");
-		error = -ENODEV;
-		goto lbl_exit;
+	if (ltc3562_dummy_write(i2c) < 0) {
+		dev_err(&i2c->dev,
+			"Could not find device LTC3562 on i2c bus\n");
+		return -ENODEV;
 	}
 
 	np = of_node_get(i2c->dev.of_node);
 	np_regulators = of_get_child_by_name(np, "regulators");
 
-	if(np_regulators == NULL){
-		dev_err(&i2c->dev, "Could not find regulators node!\n");
-		error = -EINVAL;
-		goto lbl_exit;
+	if (np_regulators == NULL) {
+		dev_err(&i2c->dev, "Could not find regulators node\n");
+		return -EINVAL;
 	}
 
-	for(i = 0; i < LTC3562_NUM_REGULATORS; ++i){
-		np_child = of_get_child_by_name(np_regulators, ltc3562_regulators[i].name);
-		if(np_child == NULL){
-			dev_err(&i2c->dev, "Could not find regulator data for %s in devicetree!\n", ltc3562_regulators[i].name);
-			error = -EINVAL;
-			goto lbl_cleanup;
-		}
+	for (i = 0; i < LTC3562_NUM_REGULATORS; ++i) {
+		np_child = of_get_child_by_name(np_regulators,
+			ltc3562_regulators[i].name);
+		if (np_child == NULL)
+			continue;
 
-		if((ltc3562_regulators[i].id == LTC3562_R400A_ID) || (ltc3562_regulators[i].id == LTC3562_R600A_ID )){
+		if ((ltc3562_regulators[i].id == LTC3562_R400A_ID) ||
+			(ltc3562_regulators[i].id == LTC3562_R600A_ID)) {
 			/* If regulator is A type get resistor values */
-			error = of_property_read_u32(np_child, "ltc3562-A-r1", &r1);
-			if(error){
-				dev_err(&i2c->dev, "Resistor value ltc3562-A-r1 for A-Type regulator %s not defined!", ltc3562_regulators[i].name);
-				goto lbl_cleanup;
+			u32 vdiv[2];
+
+			error = of_property_read_u32_array(np_child,
+				"lltc,fb-voltage-divider", vdiv, 2);
+			if (error) {
+				dev_err(&i2c->dev,
+					"Failed to parse voltage divider: %d\n",
+					error);
+				return error;
 			}
-			
-			error = of_property_read_u32(np_child, "ltc3562-A-r2", &r2);
-			if(error){
-				dev_err(&i2c->dev, "Resistor value ltc3562-A-r2 for A-Type regulator %s not defined!", ltc3562_regulators[i].name);
-				goto lbl_cleanup;
-			}
+
 			uV = ltc3562_regulators[i].min_uV / 1000;
-			ltc3562_regulators[i].min_uV = (uV + mult_frac(uV, r1, r2)) * 1000;
+			ltc3562_regulators[i].min_uV =
+				(uV + mult_frac(uV, vdiv[0], vdiv[1])) * 1000;
 
 			uV = ltc3562_regulators[i].uV_step / 1000;
-			ltc3562_regulators[i].uV_step = (uV + mult_frac(uV, r1, r2)) * 1000;
+			ltc3562_regulators[i].uV_step =
+				(uV + mult_frac(uV, vdiv[0], vdiv[1])) * 1000;
 		}
 
 		rconfig.dev = &i2c->dev;
-		rconfig.init_data = of_get_regulator_init_data(&i2c->dev, np_child);
-		rconfig.driver_data = ltc3562;	
+		rconfig.init_data =
+			of_get_regulator_init_data(&i2c->dev, np_child);
+		rconfig.driver_data = ltc3562;
 		rconfig.of_node = np_child;
-		
-		ltc3562->rstatus[i].voltage_set = 0;
 
-		ltc3562->rdev[i] = regulator_register(&ltc3562_regulators[i], &rconfig);
+		/* Set operating mode and address mask */
+		error = of_property_read_u32(np_child,
+				"lltc,operating-mode", &value);
+		if (error || (value > OPERATING_MODE_BURST))
+			value = OPERATING_MODE_PULSE_SKIP;
+		ltc3562->rstatus[i].addr_mode =
+			(PROGRAM_R400B_MASK << i) | value;
+		/* When boot-on is specified, prevent transcients by setting
+		 * the enable bit, so that setting the output voltage does not
+		 * turn off the output. */
+		if (rconfig.init_data->constraints.boot_on)
+			ltc3562->rstatus[i].enable_daccode |=
+				REGULATOR_ENABLE_BIT;
 
-		if(IS_ERR(ltc3562->rdev[i])){
+		ltc3562->rdev[i] = devm_regulator_register(&i2c->dev,
+			&ltc3562_regulators[i], &rconfig);
+
+		if (IS_ERR(ltc3562->rdev[i])) {
 			error = PTR_ERR(ltc3562->rdev[i]);
-			dev_err(&i2c->dev, "LTC3562 could not register regulator! Error # %d\n", error);			
-			goto lbl_cleanup;
+			dev_err(&i2c->dev,
+				"could not register regulator, Error %d\n",
+				error);
 		}
-	}	
+	}
 
-	i2c_set_clientdata(i2c, ltc3562);
-	dev_dbg(&i2c->dev, "LTC3562 Driver loaded.\n");
-	return 0;
-
-lbl_cleanup:
-	ltc3562_regulators_unregister(ltc3562, i);
-lbl_exit:
-	return error;
-}
-
-static int ltc3562_i2c_remove(struct i2c_client *i2c)
-{
-	struct ltc3562 *ltc3562 = i2c_get_clientdata(i2c);
-
-	ltc3562_regulators_unregister(ltc3562, LTC3562_NUM_REGULATORS);
+	dev_dbg(&i2c->dev, "LTC3562 Driver loaded\n");
 
 	return 0;
 }
+
+static const struct of_device_id ltc3562_match_id[] = {
+	{ .compatible = "lltc,ltc3562", },
+	{},
+};
+MODULE_DEVICE_TABLE(of, ltc3562_match_id);
 
 static const struct i2c_device_id ltc3562_i2c_id[] = {
-       { "ltc3562", 0 },
-       { }
+	{ "ltc3562", 0 },
+	{ }
 };
 MODULE_DEVICE_TABLE(i2c, ltc3562_i2c_id);
 
@@ -342,9 +331,9 @@ static struct i2c_driver ltc3562_i2c_driver = {
 	.driver = {
 		.name = "LTC3562",
 		.owner = THIS_MODULE,
+		.of_match_table = of_match_ptr(ltc3562_match_id),
 	},
 	.probe    = ltc3562_i2c_probe,
-	.remove   = ltc3562_i2c_remove,
 	.id_table = ltc3562_i2c_id,
 };
 
