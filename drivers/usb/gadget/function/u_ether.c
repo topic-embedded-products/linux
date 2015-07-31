c8d273dbbf533ca9ccf65790ef3e56b375052894
Mike Looijmans <mike.looijmans@topic.nl>
2015-07-31 07:59:43 +0200

    HACK: Set MTU for ethernet gadget to 512
    The ChipIdea USB driver appears to be broken. Sending more than 512 bytes
    in a single packet will 'hang' the USB driver. As a workaround, set the
    default MTU to 512 for ethernet gadgets.
    
    Intended as a temporary workaround, not a permanent fix. Though one could
    use this patch to increase the MTU and get much better RNDIS performance.

--- drivers/usb/gadget/function/u_ether.c
+++ drivers/usb/gadget/function/u_ether.c
@@ -746,6 +746,12 @@ static struct device_type gadget_type = {
 	.name	= "gadget",
 };
 
+static void gether_net_setup(struct net_device *dev)
+{
+	ether_setup(dev);
+	dev->mtu = 512; /* HACK: chipidea driver is broken */
+}
+
 /**
  * gether_setup_name - initialize one ethernet-over-usb link
  * @g: gadget to associated with these links
@@ -768,7 +774,8 @@ struct eth_dev *gether_setup_name(struct usb_gadget *g,
 	struct net_device	*net;
 	int			status;
 
-	net = alloc_etherdev(sizeof *dev);
+	net = alloc_netdev(sizeof *dev, "usb%d", NET_NAME_UNKNOWN,
+		gether_net_setup);
 	if (!net)
 		return ERR_PTR(-ENOMEM);
 
@@ -830,7 +837,8 @@ struct net_device *gether_setup_name_default(const char *netname)
 	struct net_device	*net;
 	struct eth_dev		*dev;
 
-	net = alloc_etherdev(sizeof(*dev));
+	net = alloc_netdev(sizeof *dev, "usb%d", NET_NAME_UNKNOWN,
+		gether_net_setup);
 	if (!net)
 		return ERR_PTR(-ENOMEM);
 
