56efa6481b4d73f39d32aa8747b2b2683d1cfa2d
Mike Looijmans <mike.looijmans@topic.nl>
2017-03-27 15:06:51 +0200

    firmware/psci.c: Don't register pm_power_off
    The power down on the Florida boards is implemented via an I2C GPIO
    expander. But the gpio-powerdown driver fails to load:
    poweroff-gpio gpio-poweroff: gpio_poweroff_probe: pm_power_off function already registered
    
    The problem is that PSCI already unconditionally set the pm_power_off
    to point to its own (non-functioning) implementation, so this prevents
    the board from actually powering down.
    
    There's apparently no clean way to do this, so just patch the PSCI
    driver and remove the registration.

--- drivers/firmware/psci.c
+++ drivers/firmware/psci.c
@@ -514,8 +514,6 @@ static void __init psci_0_2_set_functions(void)
 	psci_ops.migrate_info_type = psci_migrate_info_type;
 
 	arm_pm_restart = psci_sys_reset;
-
-	pm_power_off = psci_sys_poweroff;
 }
 
 /*
